a86bb633ca9bf5f713e54d6d8f34e313787185da
D.S. Ljungmark <spider@skuggor.se>
2015-06-02 18:14:30 +0200

    theme inlined
    No longer tracking upstream, because we don't want to fork everything, it works right now.
    
    the theme is a copy of pelican-bootstrap3

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,7 +35,7 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-THEME = "./pelican-bootstrap3"
+THEME = "theme"
 
 HIDE_SIDEBAR = True
 BOOTSTRAP_FLUID = False
