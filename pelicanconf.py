961dcbc63cfac6beddbd9258d782e03568b11516
D.S. Ljungmark <spider@skuggor.se>
2015-06-03 08:01:46 +0200

    Fix linebreaks in vcard

--- pelicanconf.py
+++ pelicanconf.py
@@ -73,16 +73,17 @@ BOOTSTRAP_THEME="yeti"
 BOOTSTRAP_NAVBAR_INVERSE=True
 
 ABOUT_ME="""<div class='vcard'>
-         <span class='fn'>Klas Ljungmark</span>
-         <div class='org'>Leva Ltd.</div>
-         <a class='email' href='mailto:klas@leva.blue'>klas@leva.blue</a>
-         <div class="adr">
-            <div class="street-address">123 Street Street</div>
-            <span class="locality">London</span>,
-            <abbr class="region" title="England">England</abbr>,
-            <span class="postal-code">000 999</span>
-            <div class="country-name">Great Britain</div>
-        </div>
+             <span class='fn'>Klas Ljungmark</span>
+             <div class='org'>Leva Ltd.</div>
+             <a class='email' href='mailto:klas@leva.blue'>klas@leva.blue</a>
+             <div class="adr">
+                <div class="street-address">123 Street Street</div>
+                <span class="locality">London</span>,
+                <abbr class="region" title="England">England</abbr>,
+                <span class="postal-code">000 999</span>
+                <div class="country-name">Great Britain</div>
+            </div>
+            </div>
 """
 
 DIRECT_TEMPLATES = ('index', ) # 'categories', 'authors', 'archives')
